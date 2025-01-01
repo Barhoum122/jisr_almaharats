f0704c7797b2be2019b086bb727ca19d8e3288b8
Barhoum122 <ibrahmanam122@gmail.com>
2025-01-02 00:53:06 +0300

    V1.1

---
+++ jisr_almaharat/programming_module/doctype/organization/organization.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2024, admin and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Organization", {
+	// refresh(frm) {
+    //     frappe.msgprint("hello ammar");
+
+	// },
+
+    
+    validate: function(frm){
+        
+        // to ensur the name only letters
+        const nameField = frm.doc.organization_name; 
+        const isValid = /^[A-Za-z\s]+$/.test(nameField);
+
+        if (!isValid) {
+            frappe.throw(__('Name should contain only letters and spaces.'));
+        }
+    }
+
+
+
+    
+});
